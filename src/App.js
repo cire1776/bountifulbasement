2daa21ec2e5cf73a06b411e6d1196120c803511e
cire1776 <cire1776@gmail.com>
2021-02-18 15:19:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import OrderSlip from './components/OrderSlip';
+
 
 function App() {
+  const [items, setItems] = useState({});
+  const [categories, setCategories] = useState([]);
+
+  async function fetchSlipItems() {
+    const Airtable = require('airtable-node');
+
+    console.log(process.env);
+
+    const airtable = await new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY })
+      .base('appPJGWfywLNUoPkg')
+      .table('order-slip-data');
+
+    const {records} = await airtable.list()
+    const newItems = records.reduce((accum, record)=>{
+      const category = record.fields.category;
+      
+      if (!(category in accum)) {
+        accum[category]=[];
+      }
+
+      accum[category].push(record.fields.item)
+      return accum;
+
+    },{})
+
+    setItems(newItems);
+  }
+
+  async function fetchCategories() {
+    const Airtable = require('airtable-node');
+
+    const airtable = await new Airtable({ apiKey: 'keyrXrvJEWWLSjTU2' })
+          .base('appPJGWfywLNUoPkg')
+          .table('order-slip-categories');
+
+    const {records: category_records} = await airtable.list({
+      sort: [{ field: 'sequence', direction: 'asc' }],
+    });
+
+    const newCategories = category_records.map((record)=> {
+      return record.fields.category;
+    })
+    setCategories(newCategories)
+  }
+
+  useEffect(()=>{
+    fetchCategories();
+    fetchSlipItems();
+  },[])
+
+  if (Object.entries(items).length === 0 || categories.length ===0) {
+    return <h1>Loading</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='slip-sheet'>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
+    <OrderSlip items={items} categories={categories} date={new Date().toLocaleDateString()}/>
     </div>
   );
 }
---
+++ src/components/OrderSlip.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./OrderSlip.css";
+
+function OrderSlip({items, categories, date}) {
+    const header = <header>
+            <p>Last _______________________ First ______________________</p>
+            <p># _______ # in Family ___________</p>
+            <p>☐ Walking ☐ Cart ☐ Bags ☐ No Pork</p>            
+        </header>
+
+    if (Object.keys(items).length ===0 || categories.length===0) {
+       return <h1>Loading...</h1>
+    }
+
+    return <article>
+        {header}
+
+        {
+            categories.map((category)=>{
+                return <section key={category}>
+                    <h1>{category}</h1>
+                    <div className='item-list'>
+                        <ul>
+                        {
+                            items[category].map((item) => {
+                        return (
+                            <li key={item}>___{item}</li>
+                        )
+                            })
+                        }
+                        </ul>
+                    </div>
+                </section>
+            })
+        }
+        <footer>
+            {date}
+        </footer>
+    </article>
+}
+
+export default OrderSlip
