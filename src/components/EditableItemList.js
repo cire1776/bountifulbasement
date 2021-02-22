bd7bedf46f7e14e83ec91792db2e82265baa2a14
cire1776 <cire1776@gmail.com>
2021-02-22 14:41:53 -0500

    fix conflict between calendar editor selector

--- src/components/EditableItemList.js
+++ src/components/EditableItemList.js
@@ -1,127 +1,179 @@
-import React, {useState, useEffect} from 'react'
-import pencil from '../assets/pencil.svg';
-import trashcan from '../assets/trash.svg'
-import plus from '../assets/plus.svg';
-import EditableItemName from './EditableItemName';
-import {fetchEditableItems, fetchSlipItems,fetchCategories, createItem, updateFields, deleteItem} from './order_slip_loader';
-import ModalDialog from './ModalDialog';
+import React, { useState, useEffect } from "react";
+import pencil from "../assets/pencil.svg";
+import trashcan from "../assets/trash.svg";
+import plus from "../assets/plus.svg";
+import EditableItemName from "./EditableItemName";
+import {
+  fetchEditableItems,
+  fetchSlipItems,
+  fetchCategories,
+  createItem,
+  updateFields,
+  deleteItem,
+} from "./order_slip_loader";
+import ModalDialog from "./ModalDialog";
 
-function EditableItemList({items, categories, setItems, refresher}) {
-    const [editedItem, setEditedItem] = useState(null)
-    const [deletingId, setDeletingId] = useState(null);
+function EditableItemList({ items, categories, setItems, refresher }) {
+  const [editedItem, setEditedItem] = useState(null);
+  const [deletingId, setDeletingId] = useState(null);
 
-        function toggleStatus(id) {
-        const newStatus = items[id].status === 'use' ? 'skip' : 'use';
-        items[id].status = items[id].status === 'use' ? 'skip' : 'use';
-        updateFields(id, {status: newStatus},refresher);
-        
+  function toggleStatus(id) {
+    const newStatus = items[id].status === "use" ? "skip" : "use";
+    items[id].status = items[id].status === "use" ? "skip" : "use";
+    updateFields(id, { status: newStatus }, refresher);
+  }
+
+  function changeCategory(event, id) {
+    const newCategory = event.target.value;
+    updateFields(event.target.id, { category: newCategory }, refresher);
+  }
+
+  function checkDeleteItem(id) {
+    setDeletingId(id);
+  }
+
+  function beginEditingItem(id) {
+    setEditedItem(id);
+  }
+
+  function endEditingItem(finalName) {
+    if (editedItem === 0) {
+      addItem(finalName);
+    } else {
+      changeName(editedItem, finalName);
+    }
+    refresher();
+    setEditedItem(null);
+  }
+
+  function changeName(id, newName) {
+    newName = newName.trim();
+
+    if (newName === "") {
+      return;
     }
 
-    function changeCategory(event, id) {
-        const newCategory = event.target.value;
-        updateFields(event.target.id, {category: newCategory}, refresher);
+    const item = items[id];
+    const newItem = { ...item, item: newName };
+    const newItems = (items[id] = newItem);
+    updateFields(id, { item: newName }, () => {
+      refresher();
+      setItems(items);
+    });
+  }
+
+  function addItem(newName) {
+    if (!newName) {
+      return;
     }
 
-    function checkDeleteItem(id) {
-        setDeletingId(id);
+    newName = newName.trim();
+
+    if (newName === "") {
+      return;
     }
+    createItem({
+      fields: {
+        item: newName,
+        status: "use",
+      },
+      refresher,
+    });
+  }
 
-
-    function beginEditingItem(id) {
-        setEditedItem(id);
-
-    }
-
-    function endEditingItem(finalName) {
-        if (editedItem === 0) {
-            addItem(finalName);
-        } else  {
-            changeName(editedItem, finalName);
-        }
-        refresher();
-        setEditedItem(null)
-    }
-
-   function changeName(id, newName) {
-        newName = newName.trim();
-
-        if (newName === "") {
-            return
-        }
-
-        const item = items[id];
-        const newItem = {...item, item: newName};
-        const newItems = items[id] = newItem;
-        updateFields(id, {item: newName},()=>{
-            refresher();
-            setItems(items);
-        });
-        
-    }
-
-    function addItem(newName) {
-        if (!newName) {
-            return;
-        }
-
-        newName = newName.trim();
-
-        if (newName === "") {
-            return
-        }
-        createItem({fields: {
-                            item: newName,
-                            status: 'use',
-                            },refresher
-        });
-    }
-
-    return <>
-    <ul className='list-of-items'>
+  return (
+    <>
+      <ul className="list-of-items">
         <header key="header">
-            <span>S</span>
-            <span>Name</span>
-            <span>Category</span>
+          <span>S</span>
+          <span>Name</span>
+          <span>Category</span>
         </header>
-    {
-        Object.keys(items).map((id)=> {
-            const item = items[id];
-            if (id===editedItem) {
-                return <li key={id} className={item.status === "use" ? 'used': 'skip'}>
-                            <input type='checkbox' checked={item.status === 'use'} onChange={()=>toggleStatus(id)}/>
-                            <EditableItemName id={id} name={item.name} changeName={changeName} endEditingItem={endEditingItem}/>
-                        </li>
-            } else {
-                return <li key={id} className={item.status === "use" ? 'used': 'skip'}>
-                            <input type='checkbox' checked={item.status === 'use'} onChange={()=>toggleStatus(id)}/>
-                            <h2>{item.name}</h2>
-                            <select name={id} id={id} value={item.category} onChange={(event)=>changeCategory(event, id)}>
-                                {
-                                    categories.map((category) => { 
-                                        return <option key={category} value={category}>{category} </option>
-                                    })
-                                }
-                            </select>
-                            <img src={pencil} alt="edit" onClick={(event)=>beginEditingItem(id)}/>
-                            <img src={trashcan} alt="trashcan" onClick={(event)=>{ checkDeleteItem(id) }}/>
-                        </li>
-            }
-        })
-
-    }
-    {
-        (editedItem === 0) ? <li key={0}>
-            <div></div><EditableItemName endEditingItem={endEditingItem}/>
-        </li>
-        : <button key='button' type='button' onClick={(event)=>beginEditingItem(0)}><img src={plus} alt="add"/> </button>
-    } 
-    </ul>
-    <ModalDialog activate={!!deletingId} confirmTitle="Delete" confirmAction={()=>deleteItem(deletingId, refresher)} dismiss={()=>setDeletingId(null)}>
-        <h1>Are you sure you want to delete '{deletingId && items[deletingId].name}'?
+        {Object.keys(items).map((id) => {
+          const item = items[id];
+          if (id === editedItem) {
+            return (
+              <li key={id} className={item.status === "use" ? "used" : "skip"}>
+                <input
+                  type="checkbox"
+                  checked={item.status === "use"}
+                  onChange={() => toggleStatus(id)}
+                />
+                <EditableItemName
+                  id={id}
+                  name={item.name}
+                  changeName={changeName}
+                  endEditingItem={endEditingItem}
+                />
+              </li>
+            );
+          } else {
+            return (
+              <li key={id} className={item.status === "use" ? "used" : "skip"}>
+                <input
+                  type="checkbox"
+                  checked={item.status === "use"}
+                  onChange={() => toggleStatus(id)}
+                />
+                <h2>{item.name}</h2>
+                <select
+                  name={id}
+                  id={id}
+                  value={item.category}
+                  onChange={(event) => changeCategory(event, id)}
+                >
+                  {categories.map((category) => {
+                    return (
+                      <option key={category.category} value={category.category}>
+                        {category.category}{" "}
+                      </option>
+                    );
+                  })}
+                </select>
+                <img
+                  src={pencil}
+                  alt="edit"
+                  onClick={(event) => beginEditingItem(id)}
+                />
+                <img
+                  src={trashcan}
+                  alt="trashcan"
+                  onClick={(event) => {
+                    checkDeleteItem(id);
+                  }}
+                />
+              </li>
+            );
+          }
+        })}
+        {editedItem === 0 ? (
+          <li key={0}>
+            <div></div>
+            <EditableItemName endEditingItem={endEditingItem} />
+          </li>
+        ) : (
+          <button
+            key="button"
+            type="button"
+            onClick={(event) => beginEditingItem(0)}
+          >
+            <img src={plus} alt="add" />{" "}
+          </button>
+        )}
+      </ul>
+      <ModalDialog
+        activate={!!deletingId}
+        confirmTitle="Delete"
+        confirmAction={() => deleteItem(deletingId, refresher)}
+        dismiss={() => setDeletingId(null)}
+      >
+        <h1>
+          Are you sure you want to delete '
+          {deletingId && items[deletingId].name}'?
         </h1>
-    </ModalDialog>
-</>
-
+      </ModalDialog>
+    </>
+  );
 }
 
-export default EditableItemList
+export default EditableItemList;
--- src/pages/OrderSlipEditor.js
+++ src/pages/OrderSlipEditor.js
@@ -1,43 +1,53 @@
-import React, {useEffect,useState} from 'react'
-import {fetchEditableItems, fetchSlipItems,fetchCategories, createItem, updateFields, deleteItem} from '../components/order_slip_loader';
+import React, { useEffect, useState } from "react";
+import {
+  fetchEditableItems,
+  fetchSlipItems,
+  fetchCategories,
+  createItem,
+  updateFields,
+  deleteItem,
+} from "../components/order_slip_loader";
 
-import Protected from '../components/Protected';
-import MainHeader from '../components/MainHeader';
-import OrderSlip from '../components/OrderSlip';
-import EditableItemList from '../components/EditableItemList';
-
+import Protected from "../components/Protected";
+import MainHeader from "../components/MainHeader";
+import OrderSlip from "../components/OrderSlip";
+import EditableItemList from "../components/EditableItemList";
 
 function OrderSlipEditor() {
-    const [orderSlipItems, setOrderSlipItems] = useState({});
-    const [items, setItems] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [count, setCount] = useState(0);
+  const [orderSlipItems, setOrderSlipItems] = useState({});
+  const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        fetchSlipItems(setOrderSlipItems);
-        fetchCategories(setCategories);
-        fetchEditableItems(setItems);
-    }, [count])
+  useEffect(() => {
+    fetchSlipItems(setOrderSlipItems);
+    fetchCategories(setCategories);
+    fetchEditableItems(setItems);
+  }, [count]);
 
-    useEffect(()=>{
-    },[orderSlipItems])
+  useEffect(() => {}, [orderSlipItems]);
 
-    useEffect(() => {
-    }, [items])
+  useEffect(() => {}, [items]);
 
-    if(items.length === 0 || categories.length === 0) {
-        return <h1>Loading...</h1>
-    }
-    return (
-        <Protected> 
-            <MainHeader />
-            <main>
-                <section className='editor'>
-                    <OrderSlip items={orderSlipItems} categories={categories}/>
-                    <EditableItemList items={items} categories={categories} setItems={setItems} refresher={()=>setCount((c)=>c+1)}/>
-                </section>
-            </main>
-        </Protected>)
+  if (items.length === 0 || categories.length === 0) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <Protected>
+      <MainHeader />
+      <main>
+        <section className="orderslip-editor">
+          <OrderSlip items={orderSlipItems} categories={categories} />
+          <EditableItemList
+            items={items}
+            categories={categories}
+            setItems={setItems}
+            refresher={() => setCount((c) => c + 1)}
+          />
+        </section>
+      </main>
+    </Protected>
+  );
 }
 
-export default OrderSlipEditor
+export default OrderSlipEditor;
