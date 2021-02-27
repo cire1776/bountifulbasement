3cc37363554f0235a96d6d028373ecd7914abdc8
cire1776 <cire1776@gmail.com>
2021-02-26 21:26:53 -0500

    convert to sass

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,39 +1,51 @@
-import React from 'react'
-import "../App.css"
+import React from "react";
+import "../App.scss";
 import logo from "../assets/bountifulbasementlogo.png";
 
-import netlifyIdentity from 'netlify-identity-widget';
-import AlertBar from './AlertBar'
+import netlifyIdentity from "netlify-identity-widget";
+import AlertBar from "./AlertBar";
 
 function MainHeader() {
-    return (<>
-        <AlertBar />
-        <header className='main-header'>
-            <a href="/"><img className='logo' src={logo} alt="Bountiful Basement"/></a>
-            <nav>
-                <label htmlFor="hamburger">&#9776;</label>
-                <input type="checkbox" id="hamburger"/>
+  return (
+    <>
+      <AlertBar />
+      <header className="main-header">
+        <a href="/">
+          <img className="logo" src={logo} alt="Bountiful Basement" />
+        </a>
+        <nav>
+          <label htmlFor="hamburger">&#9776;</label>
+          <input type="checkbox" id="hamburger" />
 
-                <ul>
-                    <li><a href="/about">About Us</a></li>
-                    <li><a href="/calendar">Calendar</a></li>
-                    <li><a href="/news">What's New</a></li>
-                    <li><a href="/community">Community BB</a></li>
-                    <li><a href="/difference">Making a Difference</a></li>
-                    <li><a href="/recipes">Recipes</a></li>
-                    {
-                    netlifyIdentity.currentUser() ?
-                        <li onClick={()=>netlifyIdentity.logout()}>
-                            Logout
-                        </li>
-                    :   <li onClick={()=>netlifyIdentity.open()}>
-                            Login
-                        </li>
-                    }
-                </ul>
-            </nav>
-        </header>
-    </>)
+          <ul>
+            <li>
+              <a href="/about">About Us</a>
+            </li>
+            <li>
+              <a href="/calendar">Calendar</a>
+            </li>
+            <li>
+              <a href="/news">What's New</a>
+            </li>
+            <li>
+              <a href="/community">Community BB</a>
+            </li>
+            <li>
+              <a href="/difference">Making a Difference</a>
+            </li>
+            <li>
+              <a href="/recipes">Recipes</a>
+            </li>
+            {netlifyIdentity.currentUser() ? (
+              <li onClick={() => netlifyIdentity.logout()}>Logout</li>
+            ) : (
+              <li onClick={() => netlifyIdentity.open()}>Login</li>
+            )}
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
-export default MainHeader
+export default MainHeader;
