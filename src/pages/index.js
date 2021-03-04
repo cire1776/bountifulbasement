2a4b9a04ec43ef98f689f6f37bbcf93d3437ee3f
cire1776 <cire1776@gmail.com>
2021-03-04 18:11:06 -0500

    initial home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,50 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
+import bbsign from "../assets/bb-sign 2.jpg";
 
-function Home() {
-  return <Layout></Layout>;
+function Index() {
+  return (
+    <Layout>
+      <section className="home">
+        <header className="food-insecurity">
+          <figure>
+            <article>
+              <h1>Food In•se•cu•ri•ty</h1>
+              <h2>Noun</h2>
+              <h3>Food Insecurity</h3>
+              <p>
+                The state of being without reliable access to a sufficient
+                quantity of affordable, nutritious food.
+              </p>
+              <h1>Did you know?</h1>
+              <p>
+                Thousands of residents of the West Park/Bellaire-Puritas
+                neighborhood face food insecurity at some point throughout the
+                year?
+              </p>
+            </article>
+          </figure>
+        </header>
+        <section className="bb-intro">
+          <article>
+            <p>
+              The Bountiful Basement, the Food Ministry of{" "}
+              <a href="http://blessedtrinitycleveland.org">
+                Blessed Trinity Parish
+              </a>
+              , strives to provide food security to all families in our
+              neighborhood.
+            </p>
+            <p>
+              We provide each family that we serve seven days of groceries and
+              fresh produce. Families can come once per calendar month.
+            </p>
+          </article>
+          <figure></figure>
+        </section>
+      </section>
+    </Layout>
+  );
 }
 
-export default Home;
+export default Index;
