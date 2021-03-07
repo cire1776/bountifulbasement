33bd07cc7ef7a73f1bb5772d48b139614cca1987
cire1776 <cire1776@gmail.com>
2021-03-07 14:01:52 -0500

    fix responsiveness of fooer

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -6,19 +6,25 @@ function MainFooter() {
     <footer className="main-footer">
       <section className="main">
         <a href="/"></a>
-        <div>
+        <article>
           <h2>Bountiful Basement</h2>
           <p>4370 W. 140th Street</p>
           <p>Cleveland, OH 44135</p>
           <p>(216) 671-3870</p>
-        </div>
+        </article>
+        <article>
+          <h2>Food Operations</h2>
+          <p>Tuesday: 9:30-12:00</p>
+          <p>Wednesday: 5:30-6:45</p>
+          <p>Thursday: Closed</p>
+        </article>
         <nav>
           <a href="/donate">Donate</a>
           <a href="/volunteer">Volunteer</a>
           <a href="/need-food">Need Food?</a>
         </nav>
       </section>
-      <section>
+      <section className="base-footer">
         <h1>
           © 2021 Bountiful Basement. Created by Alpha-Pegasus Media using
           Gatsby.
