8c0283a77fe8857ba606fa659d6a6398dc06b63f
cire1776 <cire1776@gmail.com>
2021-03-11 13:17:18 -0500

    link cta to need-food

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { navigate } from "gatsby";
 
 function CallToAction() {
   return (
     <section className="cta">
       <button type="button">Donate</button>
       <button type="button">Volunteer</button>
-      <button type="button">Need Food?</button>
+      <button type="button" onClick={() => navigate("/need-food/")}>
+        Need Food?
+      </button>
     </section>
   );
 }
