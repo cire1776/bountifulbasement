ef00b2e839b734251b3b8e31172f1389898cc396
cire1776 <cire1776@gmail.com>
2021-03-12 13:38:39 -0500

    add onClick to appropriate pages on buttons.

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -4,8 +4,12 @@ import { navigate } from "gatsby";
 function CallToAction() {
   return (
     <section className="cta">
-      <button type="button">Donate</button>
-      <button type="button">Volunteer</button>
+      <button type="button" onClick={() => navigate("/difference/donate/")}>
+        Donate
+      </button>
+      <button type="button" onClick={() => navigate("/difference/volunteer")}>
+        Volunteer
+      </button>
       <button type="button" onClick={() => navigate("/need-food/")}>
         Need Food?
       </button>
